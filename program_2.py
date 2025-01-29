fb642c2bdc9a4f5426a4dd980e81347e6483072d
TFoster16 <northwesterntim@gmail.com>
2025-01-29 14:53:44 -0600

    program_2.py

--- program_2.py
+++ program_2.py
@@ -1,13 +1,21 @@
+#Timothy Foster, 1/28/25, Average Age Program
 
 def average_age():
-    # Get User Input
+    # Get User Input.
+    age1 = float(input("Enter the age of your first friend."))
+    age2 = float(input("Enter the age of your second friend."))
+    age3 = float(input("Enter the age of your third friend."))
+    age4 = float(input("Enter the age of your fourth friend."))
+    age5 = float(input("Enter the age of your fifth friend."))
 
+    # Sum Ages.
+    ageSum = age1 + age2 + age3 + age4 + age5
 
-    # Sum ages
+    # Average the ages.
+    ageAverage = ageSum/5.0
 
-    # Average the ages
-
-    # Print the results
+    # Print the results.
+    print("Your friends have an average age of", ageAverage, "years.")
 
 # Line which calls the above function.
 average_age()
