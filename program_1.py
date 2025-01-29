944cb0927f29181a55fb8e324786d67e27695420
TFoster16 <northwesterntim@gmail.com>
2025-01-29 14:53:06 -0600

    program_1.py

--- program_1.py
+++ program_1.py
@@ -1,6 +1,12 @@
+#Timothy Foster, 1/27/25, Personal Information Program
+
+#Define what the function will do.
 def personal_information():
-    print('...')
+    print('Personal Information:')
+    print('Timothy Foster')
+    print('Vicksburg, NY 44098')
+    print('(322) 544-0996')
+    print('Civil Engineering')
 
-
-# Line which calls the above function.
+#Run the function.
 personal_information()
